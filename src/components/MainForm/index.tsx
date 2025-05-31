7c40bd00e80d3103057fc99ddf3c5352b0c7be61
Leandro Schillreff <leandroschillreff@gmail.com>
2025-05-31 09:23:58 -0400

    feat: add MainForm component with input, cycles, and button

---
+++ src/components/MainForm/index.tsx
@@ -0,0 +1,31 @@
+import { PlayCircleIcon } from 'lucide-react';
+import { Button } from '../Button';
+import { Cycles } from '../Cycles';
+import { Input } from '../Input';
+import styles from './styles.module.css';
+
+export const MainForm = () => {
+  return (
+    <form className={styles.form} action=''>
+      <div className={styles.formRow}>
+        <Input
+          id='myInput'
+          type='text'
+          labelText='Task:'
+          placeholder='Enter a task'
+          aria-label='Task input'
+        />
+      </div>
+      <div className={styles.formRow}>
+        <p>Lorem ipsum dolor sit amet.</p>
+      </div>
+      <div className={styles.formRow}>
+        <Cycles />
+        <p>0 0 0 0 0 0 0</p>
+      </div>
+      <div className={styles.formRow}>
+        <Button icon={<PlayCircleIcon />} />
+      </div>
+    </form>
+  );
+};
